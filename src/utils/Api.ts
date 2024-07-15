80d467796e67ae05e6531af8776b1b2a8c95fb35
WhoIsIlya <worked.mail@mail.ru>
2024-07-15 11:18:38 +0300

    Environment variables

--- src/utils/Api.ts
+++ src/utils/Api.ts
@@ -5,13 +5,13 @@ import axios from "axios";
 
 const apiBaseUrl = "https://newsapi.org/v2"
 
-const breakingNewsUrl = `${apiBaseUrl}/top-headlines?country=ru&apiKey=${apiKey}`
-const businessNewsUrl = `${apiBaseUrl}/top-headlines?country=ru&category=business&apiKey=${apiKey}`
-const technologyNewsUrl = `${apiBaseUrl}/top-headlines?country=ru&category=technology&apiKey=${apiKey}`
+const breakingNewsUrl = `${apiBaseUrl}/top-headlines?country=ru&apiKey=${process.env.EXPO_PUBLIC_API_KEY}`
+const businessNewsUrl = `${apiBaseUrl}/top-headlines?country=ru&category=business&apiKey=${process.env.EXPO_PUBLIC_API_KEY}`
+const technologyNewsUrl = `${apiBaseUrl}/top-headlines?country=ru&category=technology&apiKey=${process.env.EXPO_PUBLIC_API_KEY}`
 
-const discoverNewsUrl = (discover: any) => `${apiBaseUrl}/top-headlines?country=ru&category=${discover}&apiKey=${apiKey}`
+const discoverNewsUrl = (discover: any) => `${apiBaseUrl}/top-headlines?country=ru&category=${discover}&apiKey=${process.env.EXPO_PUBLIC_API_KEY}`
 
-const serachNewsUrl = (query: any) => `${apiBaseUrl}/top-headlines?q=${query}&apiKey=${apiKey}`
+const serachNewsUrl = (query: any) => `${apiBaseUrl}/top-headlines?q=${query}&apiKey=${process.env.EXPO_PUBLIC_API_KEY}`
 
 const apiCall = async (endpoints: any, params?: any) => {
     const options = {
